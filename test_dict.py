a48ad76e81e3312a1db5b4163b730909c8afbbe8
xinming90 <panxinming90@gmail.com>
2015-06-08 23:21:33 +0800

    add test_dict_values

--- test_dict.py
+++ test_dict.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
-class D(dict):
-    id = 10
-
-
 def test_dict_in():    
+    class D(dict):
+        id = 10
+    
     d = D()
     d.id = 10
     assert 'id' not in d
     d['id'] = 11
     assert 'id' in d
+
+
+def test_dict_values():
+    class D(dict):
+        def values(self):
+            return False
+
+    d = D()
+    d['name'] = 'eleme'
+    assert d.values() is False
+    assert dict.values(d) == ['eleme']
