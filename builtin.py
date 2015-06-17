c998bf61d9de74514206dd30e65e051a15f65160
xinming90 <panxinming90@gmail.com>
2015-06-17 22:56:57 +0800

    add builtin any

--- builtin.py
+++ builtin.py
@@ -12,3 +12,15 @@ def filter(function_or_none, sequence): # known special case of filter
     if function_or_none in (None, bool):
         return [item for item in sequence if item]
     return [item for item in sequence if function_or_none(item)]
+
+def any(iterable): # real signature unknown; restored from __doc__
+    """
+    any(iterable) -> bool
+    
+    Return True if bool(x) is True for any x in the iterable.
+    If the iterable is empty, return False.
+    """
+    for item in iterable:
+        if item:
+            return True
+    return False
--- test_builtin.py
+++ test_builtin.py
@@ -8,3 +8,8 @@ def test_filter():
 
     assert filter(bool, [1, 0, 2]) == [1, 2]
     assert builtin.filter(bool, [1, 0, 2]) == [1, 2]
+
+
+def test_any():
+    assert any([1, 0, 2]) == True
+    assert builtin.any([1, 0, 2]) == True
