6eb1796a0a8915744a792c33eaa7467cde95a094
xinming90 <panxinming90@gmail.com>
2015-06-24 23:42:24 +0800

    add test_operator.py

---
+++ test_operator.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import operator
+
+
+def test_iadd():
+    class C(object):
+        def __add__(self, other):
+            return '__add__'
+
+        def __iadd__(self, other):
+            return '__iadd__'
+
+    c = C()
+    # c + 5
+    assert operator.add(c, 5) == "__add__"
+    # c += 5
+    assert operator.iadd(c, 5) == "__iadd__"
+
+
+def test_list_add_iadd_diff():
+    l2 = l1 = l = [1, 2]
+    l1 = l1 + [3, 4]
+    assert id(l) != id(l1)
+    l2 += [3, 4]
+    assert id(l) == id(l2)
