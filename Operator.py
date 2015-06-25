cbdc714df34d59bccae90748bf1a62a4441287c1
xinming90 <panxinming90@gmail.com>
2015-06-25 14:26:19 +0800

    add Operator.py

---
+++ Operator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+def add(a, b):
+    """ add(a, b) -- Same as a + b. """
+    if hasattr(a, '__add__'):
+        return a.__add__(b)
+
+
+def iadd(a, b):
+    """ a = iadd(a, b) -- Same as a += b. """
+    if hasattr(a, '__iadd__'):
+        return a.__iadd__(b)
+    if hasattr(a, '__add__'):
+        return a.__add__(b)
+
--- test_operator.py
+++ test_operator.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import operator
-
+import Operator
 
 def test_iadd():
     class C(object):
@@ -14,10 +14,19 @@ def test_iadd():
     c = C()
     # c + 5
     assert operator.add(c, 5) == "__add__"
+    assert Operator.add(c, 5) == '__add__'
     # c += 5
     assert operator.iadd(c, 5) == "__iadd__"
+    assert Operator.iadd(c, 5) == '__iadd__'
 
+    class C(object):
+        def __add__(self, other):
+            return '__add__'
+    c = C()
+    assert operator.iadd(c, 5) == "__add__"
+    assert Operator.iadd(c, 5) == '__add__'
 
+    
 def test_list_add_iadd_diff():
     l2 = l1 = l = [1, 2]
     l1 = l1 + [3, 4]
