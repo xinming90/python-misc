0ef3e97bb64e8fee0556c7511b4be2e956e20072
xinming90 <panxinming90@gmail.com>
2015-07-04 21:41:51 +0800

    add types lambda code ...

--- _typesmodule.c
+++ _typesmodule.c
@@ -6,4 +6,11 @@ init_types(void)
     PyObject *m;
     m = Py_InitModule("_types", NULL);
     PyModule_AddObject(m, "FunctionType", &PyFunction_Type);
+    PyModule_AddObject(m, "LambdaType", &PyFunction_Type);
+    PyModule_AddObject(m, "CodeType", &PyCode_Type);
+    PyModule_AddObject(m, "GeneratorType", &PyGen_Type);
+    PyModule_AddObject(m, "ClassType", &PyClass_Type);
+    PyModule_AddObject(m, "ModuleType", &PyModule_Type);
+    PyModule_AddObject(m, "TypeType", &PyType_Type);
+    PyModule_AddObject(m, "DictProxyType", &PyDictProxy_Type);
 }
--- test_types.py
+++ test_types.py
@@ -3,9 +3,51 @@
 import types
 import _types
 
-def test_function():
+def test_function_type():
     def f():
         pass
 
     assert type(f) is types.FunctionType
     assert type(f) is _types.FunctionType
+
+
+def test_lambda_type():
+    f = lambda: None
+    assert type(f) is types.LambdaType
+    assert type(f) is _types.LambdaType
+
+
+def test_code_type():
+    def f():
+        pass
+    assert type(f.func_code) is types.CodeType
+    assert type(f.func_code) is _types.CodeType
+
+
+def test_generator_type():
+    def g():
+        yield
+    assert type(g()) is types.GeneratorType
+    assert type(g()) is _types.GeneratorType
+
+
+def test_class_type():
+    class C:
+        pass
+    assert type(C) is types.ClassType
+    assert type(C) is _types.ClassType
+
+
+def test_module_type():
+    assert type(types) is types.ModuleType
+    assert type(types) is _types.ModuleType
+
+
+def test_type_type():
+    assert type(type) is types.TypeType
+    assert type(type) is _types.TypeType
+
+
+def test_dict_proxy_type():
+    assert type(type.__dict__) is types.DictProxyType
+    assert type(type.__dict__) is _types.DictProxyType
