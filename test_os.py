782992637a08164b0281bd8313aac29fa6f969a6
xinming90 <panxinming90@gmail.com>
2015-07-07 23:51:13 +0800

    update test_os_mkdir

--- test_os.py
+++ test_os.py
@@ -9,14 +9,15 @@ def test_os_mkdir():
     os.umask(002)
 
     # man 2 umask
+    # man 2 mkdir
     os.mkdir('tmp', 0777)
     assert oct(os.stat('tmp').st_mode & 4095) == '0775'
-    assert oct(os.stat('tmp').st_mode & 4095 & ~002 & 0777) == '0775'
+    assert oct(~002 & 0777) == '0775'
     os.rmdir('tmp')
 
     os.umask(000)
     os.mkdir('tmp', 0777)
     assert oct(os.stat('tmp').st_mode & 4095) == '0777'
-    assert oct(os.stat('tmp').st_mode & 4095 & ~000 & 0777) == '0777'
+    assert oct(~000 & 0775) == '0775'
     os.rmdir('tmp')
     os.rmdir('/tmp/dir')
