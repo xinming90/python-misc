e1f1bd3b1ab1a7b213482e303dd30d6861311225
xinming90 <panxinming90@gmail.com>
2015-07-14 14:48:30 +0800

    add test_pytest_raises_regexp

--- test_pytest.py
+++ test_pytest.py
@@ -7,6 +7,16 @@ def test_pytest_raises():
     def f():
         raise ValueError('f')
 
+    pytest.raises(ValueError, f)
     with pytest.raises(ValueError) as e:
         f()
     assert e.value.message == 'f'
+
+
+def test_pytest_raises_regexp():
+    def f():
+        raise ValueError('f')
+
+    pytest.raises_regexp(ValueError, 'f', f)
+    with pytest.raises_regexp(ValueError, 'f') as e:
+        f()
