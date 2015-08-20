3af309651ae9a5841d415bd6b825395b6801305f
xinming90 <panxinming90@gmail.com>
2015-08-20 16:14:24 +0800

    add unpatch_gevent_all

--- scripts/profile.py
+++ scripts/profile.py
@@ -8,6 +8,22 @@ import cProfile
 import StringIO
 
 
+def unpatch_gevent_all():
+    do_nothing = lambda *args, **kwds: None
+    import gevent.monkey
+    setattr(gevent.monkey, 'patch_all', do_nothing)
+    setattr(gevent.monkey, 'patch_os', do_nothing)
+    setattr(gevent.monkey, 'patch_time', do_nothing)
+    setattr(gevent.monkey, 'patch_thread', do_nothing)
+    setattr(gevent.monkey, 'patch_sys', do_nothing)
+    setattr(gevent.monkey, 'patch_socket', do_nothing)
+    setattr(gevent.monkey, 'patch_select', do_nothing)
+    setattr(gevent.monkey, 'patch_ssl', do_nothing)
+    setattr(gevent.monkey, 'patch_subprocess', do_nothing)
+
+unpatch_gevent_all()
+
+
 @contextlib.contextmanager
 def profile(sort_by="cumulative"):
     p = cProfile.Profile()
