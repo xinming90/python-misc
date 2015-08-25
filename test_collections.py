9a5dccc52cf417c345628c0d040ca5d5e930201a
xinming90 <panxinming90@gmail.com>
2015-08-25 20:02:24 +0800

    add test_OrderedDict

--- test_collections.py
+++ test_collections.py
@@ -8,3 +8,18 @@ def test_collections_defaultdict():
     assert dict(d) == {}
     d['name']
     assert dict(d) == {'name': 0}
+
+
+def test_OrderedDict():
+    d = {}
+    d[5] = 5
+    d[0] = 0
+    assert d.keys() == [0, 5]
+
+    od = collections.OrderedDict()
+    od[5] = 5
+    od[0] = 0
+    assert od.keys() == [5, 0]
+
+    od[5] = 15
+    assert od.keys() == [5, 0]
