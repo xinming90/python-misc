e8784e2b7bb06393d5c0d2fd1bae7c6c94d14901
xinming90 <panxinming90@gmail.com>
2015-08-27 01:05:24 +0800

    use pipeline in rdb_to_twemproxy.py

--- scripts/rdb_to_twemproxy.py
+++ scripts/rdb_to_twemproxy.py
@@ -10,32 +10,46 @@ now = datetime.datetime.now
 class MyCallback(RdbCallback):
     def __init__(self, r):
         self.redis = r
+        self.count = 0
+        self.p = r.pipeline(transaction=False)
 
     def set(self, key, value, expiry, info):
         print "set|{}|{}".format(key, value)
-        # self.redis.set(key, value, expiry)
-        self.redis.set(key, value)
+        self.p.set(key, value)
+        self.execute()
 
     def hset(self, key, field, value):
         print "hset|{}|{}|{}".format(key, field, value)
-        self.redis.hset(key, field, value)
+        self.p.hset(key, field, value)
+        self.execute()
 
     def sadd(self, key, member):
         print "sadd|{}|{}".format(key, member)
         self.redis.sadd(key, member)
+        self.execute()
 
     def rpush(self, key, value) :
         print "rpush|{}|{}".format(key, value)
         self.redis.rpush(key, value)
+        self.execute()
 
     def zadd(self, key, score, member):
         print "zadd|{}|{}|{}".format(key, member, score)
         self.redis.zadd(key, member, score)
+        self.execute()
+
+    def end_rdb(self):
+        self.p.execute()
+
+    def execute(self):
+        self.count = (self.count + 1) % 10000
+        if not self.count:
+            self.p.execute()
 
 
 r = redis.Redis.from_url("redis://127.0.0.1:6400")
 callback = MyCallback(r)
-parser = RdbParser(callback)
+parser = RdbParser(callback, filters={"dbs": [0]})
 print "start {}".format(now())
-parser.parse('/home/xinming.pan/dump.rdb')
+parser.parse("/home/xinming.pan/dump.rdb")
 print "stop {}".format(now())
