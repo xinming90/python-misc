0d07678f965428501e0b57654607d61387ad460d
xinming90 <panxinming90@gmail.com>
2017-05-08 21:23:19 +0800

    add test_collections_defaultdict_list

--- test_collections.py
+++ test_collections.py
@@ -10,6 +10,29 @@ def test_collections_defaultdict():
     assert dict(d) == {'name': 0}
 
 
+def test_collections_defaultdict_list():
+    names = [
+        'raymond', 'rachel', 'matthew', 'roger',
+        'betty', 'melissa', 'judith', 'charlie',
+    ]
+    def f1(names):
+        d = {}
+        for name in names:
+            key = len(name)
+            if key not in d:
+                d[key] = []
+            d[key].append(name)
+        return d
+
+    def f2(names):
+        d = collections.defaultdict(list)
+        for name in names:
+            d[len(name)].append(name)
+        return d
+
+    assert f1(names) == f2(names)
+
+
 def test_OrderedDict():
     d = {}
     d[5] = 5
