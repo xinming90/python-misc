b9938a3e114d9370ffee91383b1fd511fb8ff4b3
xinming90 <panxinming90@gmail.com>
2017-12-04 15:33:46 +0800

    add test_isgeneratorfunction

--- test_generator.py
+++ test_generator.py
@@ -3,6 +3,16 @@
 import pytest
 
 
+CO_GENERATOR = 0x0020
+
+
 def test_generator_len():
     with pytest.raises(TypeError):
         len(i for i in range(10))
+
+
+def test_isgeneratorfunction():
+    def gen():
+        yield 1
+
+    assert gen.__code__.co_flags & CO_GENERATOR
