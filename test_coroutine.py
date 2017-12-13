3ff538243d79e46f8a6d56666683082b2fb72332
xinming90 <panxinming90@gmail.com>
2017-12-13 13:55:59 +0800

    add test_iter_coroutine

--- test_coroutine.py
+++ test_coroutine.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+
 
 CO_COROUTINE = 0x0080
 
@@ -9,3 +11,12 @@ def test_iscoroutinefunction():
         pass
 
     assert coro.__code__.co_flags & CO_COROUTINE
+
+
+def test_iter_coroutine():
+    async def coro():
+        pass
+
+    c = coro()
+    with pytest.raises(TypeError):
+        iter(c)
